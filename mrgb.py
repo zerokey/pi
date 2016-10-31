86c79ed44aab2141d6ca3e9ec35c5837b18eea11
Jason Brown <jbrown@jbrown-mbp.local>
2016-10-31 11:18:13 -0700

    init

---
+++ mrgb.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+import time
+import morse
+
+data_in = raw_input("What would you like to transmit? ")
+
+red = 18
+green = 17
+blue = 27
+led_time = 1
+RUNNING = True
+
+# setup pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+for led in red, green, blue:
+    GPIO.setup(led, GPIO.OUT)
+
+#GPIO.setup(green,GPIO.OUT)
+#GPIO.setup(red,GPIO.OUT)
+#GPIO.setup(blue,GPIO.OUT)
+
+def led_out(color, sleep_time):
+    GPIO.output(color,GPIO.LOW)
+    time.sleep(sleep_time)
+    GPIO.output(color,GPIO.HIGH)
+    time.sleep(sleep_time)
+		
+words = morse.string_to_morse(data_in)
+
+def lights():
+	for signal in range(0, len(words)):
+		if '-' in words[signal]: 
+			led_out(red, .25)
+		
+		if '.' in words[signal]: 
+			led_out(green, .125)
+		
+		if ' ' in words[signal]: 
+			led_out(blue, .5)
+
+try:
+	for i in range(1,10):
+		lights()
+
+except KeyboardInterrupt:
+    RUNNING = False
+    print "Quitting\n"
+finally:
+	GPIO.cleanup()
